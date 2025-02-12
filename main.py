345d571cc63bc6c0da249db4f3824e03c8e88e7d
DUAAAA <501509035@qq.com>
2025-02-12 14:10:29 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
-from astrbot.api import logger
+from astrbot.api.event import filter, AstrMessageEvent
+from astrbot.api.event.filter import event_message_type, EventMessageType
 
-@register("helloworld", "Your Name", "一个简单的 Hello World 插件", "1.0.0", "repo url")
-class MyPlugin(Star):
+@register("astrbot_plugin_plus_one", "DUAAAA", "群聊相同文本消息自动+1的插件", "1.0.0", "https://github.com/xia-rihua/astrbot_plugin_plus_one")
+class RepeatSamePlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
-    
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+        self.last_message_state = {}  # 记录每个群的最后一条消息状态
+
+    def update_state(self, group_id, text, triggered=False):
+        """更新群聊状态信息"""
+        self.last_message_state[group_id] = {"text": text, "triggered": triggered}
+
+    @filter.event_message_type(EventMessageType.GROUP_MESSAGE)
+    async def on_group_message(self, event: AstrMessageEvent):
+        if event.get_sender_id() == event.message_obj.self_id:
+            return  # 避免处理机器人自己发送的消息
+
+        group_id = event.message_obj.group_id
+        current_text = event.message_str.strip()
+        if not current_text:
+            return  # 忽略空消息
+
+        last_state = self.last_message_state.get(group_id, {"text": None, "triggered": False})
+
+        if last_state["text"] == current_text:
+            if not last_state["triggered"]:
+                yield event.plain_result(current_text)
+                self.update_state(group_id, current_text, triggered=True)
+        else:
+            self.update_state(group_id, current_text)
